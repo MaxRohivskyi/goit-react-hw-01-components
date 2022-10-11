f007e4fb270f53407ce81bd85c4b92fb33ae2f7f
Max Rohivskyi <maxrogivsky@gmail.com>
2022-10-12 01:14:48 +0300

    add-propTypes-in-component-FriendListItem

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { FriendItemName, FriendItemImg, FriendItemStatus, FriendItemWrapper } from './FriendListItem.styled';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
@@ -8,6 +9,16 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
       <FriendItemName>{name}</FriendItemName>
     </FriendItemWrapper>
   );
+};
+
+FriendListItem.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
 };
 
 
